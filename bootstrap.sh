18df282572634b1f6bfffcdb70334dab426b7a39
max furman <mx.furman@gmail.com>
2015-10-28 16:44:51 -0700

    removed patches, bumped to 0.9.13
    * 0.9.13 has the patches that we were manually doing before

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 # Run this in a path you don't care about, things may get deleted!
-VERSION="0.9.12"
-BUILD="betable3"
+VERSION="0.9.13"
+BUILD="betable2"
 
 set -e -x
 ORIGPWD="$(pwd)"
@@ -8,34 +8,33 @@ TMP="$(mktemp -d)"
 cd $TMP
 trap "rm -rf \"$TMP\"" EXIT INT QUIT TERM
 
-git clone --depth 1 git@github.com:graphite-project/graphite-web.git
+git clone git@github.com:graphite-project/graphite-web.git
 cd graphite-web
-git checkout "tags/$VERSION"
-
-# Apply patches
-patch -p1 < "$ORIGPWD/patches/graphite-syslogger.patch"
-patch -p1 < "$ORIGPWD/patches/safe-unpickle.patch"
+git checkout -B "$VERSION" "aa37f1bd3f3d2478b3d84ff20bd309db3ec30a02"
 
 # Stupid hack for new django
 sed -i.bak "s/from django\.conf\.urls\.defaults import \*/from django.conf.urls import patterns, url, include/g" webapp/graphite/urls.py
 sed -i.bak "s/from django\.conf\.urls\.defaults import \*/from django.conf.urls import patterns, url, include/g" webapp/graphite/render/urls.py
 sed -i.bak "s/from django\.conf\.urls\.defaults import \*/from django.conf.urls import patterns, url, include/g" webapp/graphite/metrics/urls.py
 sed -i.bak "s/from django\.conf\.urls\.defaults import \*/from django.conf.urls import patterns, url, include/g" webapp/graphite/dashboard/urls.py
-sed -i.bak "s/from django\.conf\.urls\.defaults import \*/from django.conf.urls import patterns, url, include/g" webapp/graphite/cli/urls.py
 sed -i.bak "s/from django\.conf\.urls\.defaults import \*/from django.conf.urls import patterns, url, include/g" webapp/graphite/version/urls.py
 sed -i.bak "s/from django\.conf\.urls\.defaults import \*/from django.conf.urls import patterns, url, include/g" webapp/graphite/browser/urls.py
 sed -i.bak "s/from django\.conf\.urls\.defaults import \*/from django.conf.urls import patterns, url, include/g" webapp/graphite/account/urls.py
-sed -i.bak "s/from django\.conf\.urls\.defaults import \*/from django.conf.urls import patterns, url, include/g" webapp/graphite/graphlot/urls.py
 sed -i.bak "s/from django\.conf\.urls\.defaults import \*/from django.conf.urls import patterns, url, include/g" webapp/graphite/composer/urls.py
 sed -i.bak "s/from django\.conf\.urls\.defaults import \*/from django.conf.urls import patterns, url, include/g" webapp/graphite/whitelist/urls.py
 sed -i.bak "s/from django\.conf\.urls\.defaults import \*/from django.conf.urls import patterns, url, include/g" webapp/graphite/events/urls.py
 python setup.py install --install-data $TMP/prepare/var/lib/graphite --install-lib $TMP/prepare/opt/graphite/webapp --prefix $TMP/prepare/opt/graphite
-cd ../prepare
+cd ../
+# Putting static files in place
+mkdir -p prepare/var/www; mkdir -p prepare/var/www/graphite
+cp -r graphite-web/webapp/content/ prepare/var/www/graphite/static
+
+cd prepare
 
 rm -f "$ORIGPWD/graphite-web_${VERSION}-${BUILD}_amd64.deb"
 
 fakeroot fpm -m "Nate Brown <nate@betable.com>" \
              -n "graphite-web" -v "$VERSION-$BUILD" \
              -p "$ORIGPWD/graphite-web_${VERSION}-${BUILD}_amd64.deb" \
-             -d "whisper = 0.9.12" -d "carbon = 0.9.12" \
+             -d "whisper = $VERSION" -d "carbon = $VERSION" \
              -s "dir" -t "deb" "."
